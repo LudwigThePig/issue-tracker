d5121d2365230e749420d91f14e738b33216eb32
Morgan Galvin <morgangalvin@yahoo.com>
2019-01-23 07:37:19 +1300

    installed dependencies and added readme

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
-**FreeCodeCamp**- Information Security and Quality Assurance
-------
+# Project Issue Tracker
 
-Project Issue Tracker
+With this app you can create a project, post issues associated with those problems, update/close those issues, and delete the issues.
 
-1) SET NODE_ENV to `test` without quotes and set DB to your mongo connection string in .env file
-2) Complete the project in `routes/api.js` or by creating a handler/controller
-3) You will add any security features to `server.js`
-4) You will create all of the functional tests in `tests/2_functional-tests.js`
+## Tech Used
+- Node.js
+- Express
+- Chai and Mocha
+- Zombie
+- MongoDB
+- Helmet
+- Zombie.js
 
-
+[Click here to see a demo](https://hissing-lynx.glitch.me/)
--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,3 @@
-/*
-*
-*
-*       Complete the API routing below
-*
-*
-*/
-
 'use strict';
 
 var expect = require('chai').expect;
@@ -15,11 +7,19 @@ var ObjectId = require('mongodb').ObjectID;
 const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
 
 module.exports = function (app) {
+  MongoClient.connect(process.env.MONGO, (err)=>{
+    if (err){
+      console.log(`Ooops, looks like something went wrong: ${err}`);
+    } else {
+      console.log(`You are now connected to the database!`)
+    }
+  });
 
   app.route('/api/issues/:project')
   
     .get(function (req, res){
-      var project = req.params.project;
+      var project = req.params;
+      console.log(project);
       
     })
     
