f4ee26b7d673f5a7437f4e2ebd509637b3338af6
Morgan Galvin <morgangalvin@yahoo.com>
2019-01-25 10:42:52 +1300

    set up api fetch for project names

--- public/index.js
+++ public/index.js
@@ -1,42 +1,45 @@
-window.onload = function(){
-    var request = new XMLHttpRequest();
-    request.open('GET', '/api/projects', true);
+( function(){
   
-    request.onload = function() {
-      if (request.status >= 200 && request.status < 400) {
-        console.log('we made it');
-        var resp = request.responseText;
-      } else {
-        // We reached our target server, but it returned an error
-  
+    //fetch projects and store in list
+    let projects = {  
+      list: [],
+      fetchProjects: function(){
+        return fetch('/api/projects')
+          .then(function(res) {
+            return res.json();
+          })
+          .then(res => {
+            this.list = res.projects.map(project => [project.project_name, project._id]);
+            return;
+          })
+          .catch(function(err) {
+            throw new Error('oops something went wrong');
+          });
+      },
+      addProject: function(){
+        //post something
       }
+      
     };
-  
-    request.onerror = function() {
-      // There was a connection error of some sort
-    };
-  
-    request.send();
-  
-    console.log(request)
-  
-    let projects = [];
+    
+    console.log(projects.list);
   
     //Creating DOM elements for each project
     let project;
-    for (project in projects){
+    for (project in projects.list){
+      const linkWrapper = document.createElement('a');
       const projDiv = document.createElement('div');
       const projTitle = document.createElement('h2');
       const issueCount = document.createElement('p');
   
+      linkWrapper.setAttribute('src', `issues/${project}`);
       projDiv.setAttribute('class', 'projectDiv');
-      projTitle.innerText = project.name;
-      issueCount.innerText = `${project.issues.length} issues reported`;
+      projTitle.innerText = project;
+      issueCount.innerText = `${project.issues.length()} issues reported`;
   
       projDiv.appendChild(projTitle);
       projDiv.appendChild(issueCount);
   
-      document.getElementById('main').appendChild(projDiv);
+      document.getElementById('main').appendChild(linkWrapper);
     }
-  }// end onload
-  
+  })()// end iife  
